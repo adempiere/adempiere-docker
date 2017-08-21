68c0500e81974359bd953249e56ee2dc7733dd3c
Raúl Capecce <raul.capecce@openup.com.uy>
2017-08-21 15:13:58 -0300

    Changes on Adempiere start

--- adempiere-last/start-adempiere.sh
+++ adempiere-last/start-adempiere.sh
@@ -21,23 +21,47 @@ until psql -h $ADEMPIERE_DB_HOST -p $ADEMPIERE_DB_PORT -U "postgres" -c '\l'; do
   sleep 1
 done
 
+# Run Setup if it hasn't been runned
 if [ "$AD_SETED_CONFIGURATION" != "Y" ]; then
 
   cd /opt/Adempiere
   ./RUN_silentsetup.sh
 
-  if [ "$ADEMPIERE_DB_INIT" = "Y" ]; then
-    echo "============ INIT DB ============"
+  AD_SETED_CONFIGURATION=Y
+fi
+
+# Run initialization of the data base
+if [ "$ADEMPIERE_DB_INIT" = "Y" ]; then
+  if [ "$ALREADY_ADEMPIERE_DB_INIT" = "Y" ]; then
+    echo "================================================================================"
+    echo "==                        * * *   WARNING  * * *                              =="
+    echo "== Database already initialized                                               =="
+    echo "== Please disable ADEMPIERE_DB_INIT argument in docker-compose.yml file       =="
+    echo "== If you want to initialize the database again, first start the container    =="
+    echo "== with this argument in N and then start it with this argument in Y          =="
+    echo "================================================================================"
+  else
+    echo "================================================================================"
+    echo "==                      * * *   RESTORING DB  * * *                           =="
+    echo "================================================================================"
     cd /opt/Adempiere/utils
     psql -h $ADEMPIERE_DB_HOST -p $ADEMPIERE_DB_PORT -U postgres -c "DROP ROLE IF EXISTS adempiere"
     psql -h $ADEMPIERE_DB_HOST -p $ADEMPIERE_DB_PORT -U postgres -c "CREATE ROLE adempiere LOGIN PASSWORD '$ADEMPIERE_DB_PASSWORD'"
     Y|./RUN_ImportAdempiere.sh
-  else
-    echo "========== NO INIT DB ==========="
+    ALREADY_ADEMPIERE_DB_INIT=Y
   fi
-
-  echo "AD_SETED_CONFIGURATION=Y" > $ENV_FILE
+else
+  echo "================================================================================"
+  echo "==                      * * *   DB NOT RESTORED  * * *                        =="
+  echo "================================================================================"
+  ALREADY_ADEMPIERE_DB_INIT=N
 fi
+
+# Setting environment docker file
+echo "# ADempiere Docker Environment File" > $ENV_FILE
+echo "AD_SETED_CONFIGURATION=$AD_SETED_CONFIGURATION" >> $ENV_FILE
+echo "ALREADY_ADEMPIERE_DB_INIT=$ALREADY_ADEMPIERE_DB_INIT" >> $ENV_FILE
+
 
 cd /opt/Adempiere/utils
 ./RUN_Server2.sh
