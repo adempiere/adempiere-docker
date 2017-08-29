aea3f0afa4c025b937edbf38877739f241ec98d3
Sergio Ramazzina <sergio.ramazzina@serasoft.it>
2017-08-29 18:29:32 +0200

    Fixed a missing comma

--- postgresql/pg_setup_firsttime.sql
+++ postgresql/pg_setup_firsttime.sql
@@ -1,5 +1,5 @@
 ALTER USER postgres WITH PASSWORD 'postgres';
-DROP USER adempiere
+DROP USER adempiere;
 CREATE USER adempiere WITH SUPERUSER PASSWORD 'adempiere';
 UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
 DROP DATABASE template1;
